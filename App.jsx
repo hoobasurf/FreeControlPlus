63a8bf8898e82fb3f9f75c70b921db9cffa3b32b
hoobasurf <brice.paneela@icloud.com>
2025-06-01 15:23:02 +0200

    FreeControl

---
+++ App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>🔧 FreeControl+</h1>
+      <p>Bienvenue dans votre centre de contrôle réseau.</p>
+    </div>
+  )
+}
